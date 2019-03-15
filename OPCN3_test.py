68c1d2c4210e25387496f5e6acfcb335550ba952
JarvisSan22 <45226060+JarvisSan22@users.noreply.github.com>
2019-03-15 09:21:05 +0000

    Update OPCN3_test.py
    -Working: Fan and Lazer on and off
    -Not working get hist, due to the ser.read(86) no returning the 86 data points

--- OPCN3_test.py
+++ OPCN3_test.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar  5 10:41:45 2019
+Created on Wed Mar 15 11:59:10 2019
 
-@author: Jarvi
-OPCN3 test code 
+@author: Daniel Jarvis
+Note: Able to turn fan and Lazer on and off, but still cant getHist
 """
 
 
@@ -14,20 +14,16 @@ import struct
 import datetime
 import sys
 import os.path
-#IMport the file names, you dont want to type them out in all the code 
 
-import variables as V
-# SAMPLING VARIABLES
-#integration time (seconds)
 integration=5
 
 # NAMING VARIABLES
 OPCNAME = "TestOPC"
 OPCPORT= "COM8"
 LOCATION = "Lab2"
-#FOLDER = "C:\Users\Jarvi\Documents\MRES!!!!!!! 勝とう\Project\Pi\26-2-2019 pi code\AQ\OPCData"
-
-# Init
+wait=0.00001
+#
+# Init OPC for spi connection 
 
 def initOPC(ser):
         #print("Init:")
@@ -35,170 +31,311 @@ def initOPC(ser):
         ser.write(bytearray([0x5A,0x01]))
         nl = ser.read(3)
         print(nl)
-        time.sleep(.1)
+        time.sleep(wait)
         ser.write(bytearray([0x5A,0x03]))
         nl=ser.read(9)
         print(nl)
-        time.sleep(.1)
+        time.sleep(wait)
 		
 		 #SPI conncetion
         ser.write(bytearray([0x5A,0x02,0x92,0x07]))
         nl=ser.read(2)
         print(nl)
-        time.sleep(.1)
+        time.sleep(wait)
         
        
 # Turn fan 
         
 def fanOff(ser):
-        print("Fan off")
+        print("Fan turn off")
+        
          #start the flow chart the flow chart
-     #   ser.write(bytearray([0x61,0x03]))
-        time.sleep(.1)
-        ser.write(bytearray([0x61,0x03]))
-        nl = ser.read(2)
-        print(nl)
-        time.sleep(.1)
-        #fan off
-        ser.write(bytearray([0x61,0x02]))
-        nl = ser.read(2)
-        print(nl)
-        time.sleep(.1)
-     
+        T=0 #Triese counter
+        while True:
+            
+            ser.write(bytearray([0x61,0x03]))
+            nl = ser.read(2)
+            print(nl)
+            T=T+1 
+            if nl== (b"\xff\xf3" or b"xf3\xff"):
+                time.sleep(wait)
+                #fan off
+                ser.write(bytearray([0x61,0x02]))
+                nl = ser.read(2)
+                print(nl)
+                time.sleep(2)
+                fan="OFF"
+                print("Fan off")
+                return fan
+            elif T > 20:
+                
+                print("Reset SPI")
+                time.sleep(3) #time for spi buffer to reset
+                #reset SPI  conncetion 
+                #initOPC(ser)
+                T=0
+            else:
+                time.sleep(wait*10) #wait 1e-05 before next commnad 
+                            
+             
 # Turn fan and laser on
 def fanOn(ser):
-        print("Fan ON")
-        
-        ser.write(bytearray([0x61,0x03]))
-        nl = ser.read(2)
-        print(nl)
-        time.sleep(.1)
-        ser.write(bytearray([0x61,0x03]))
-        nl = ser.read(2)
-        print(nl)
-        time.sleep(.1)
-
+        print("Fan turn on")
+        #start the flow chart the flow chart
+        T=0 #Triese counter
+        while True:   
+            ser.write(bytearray([0x61,0x03]))
+            nl = ser.read(2)
+            print(nl)
+            T=T+1 
+            if nl== (b"\xff\xf3" or b"xf3\xff"):
+                time.sleep(wait)
+                #fan on
+                ser.write(bytearray([0x61,0x03]))
+                nl = ser.read(2)
+                print(nl)
+                time.sleep(2)
+                fan="ON"
+                print("Fan On")
+                return fan
+            elif T > 20:
+                print("Reset SPI")
+                time.sleep(3) #time for spi buffer to reset
+                #reset SPI  conncetion 
+               # initOPC(ser)
+                T=0
+            else:
+                time.sleep(wait*10) #wait 1e-05 before next commnad 
+                        
 #Lazer on   0x07 is SPI byte following 0x03 to turn laser ON.
 def LazOn(ser):
-        print("Lazer On")
-        #ser.write(bytearray([0x61,0x03]))
-        time.sleep(.1)
-        ser.write(bytearray([0x61,0x03]))
-        nl = ser.read(2)
-        print(nl)
-        time.sleep(.1)
-        ser.write(bytearray([0x61,0x07])) #ON
-        nl = ser.read(2)
-        print(nl)
-        time.sleep(.1)
+        print("Lazer turn On")
+        T=0 #Triese counter
+        while True:   
+            ser.write(bytearray([0x61,0x03]))
+            nl = ser.read(2)
+            print(nl)
+           
+            T=T+1 
+            if nl== (b"\xff\xf3" or b"xf3\xff"):
+                time.sleep(wait)
+                #Lazer on
+                ser.write(bytearray([0x61,0x07]))
+                nl = ser.read(2)
+                print(nl)
+                time.sleep(wait)
+                Laz="ON"
+                print("Fan On")
+                return Laz
+            elif T > 20:
+                print("Reset SPI")
+                time.sleep(3) #time for spi buffer to reset
+                #reset SPI  conncetion 
+              #  initOPC(ser)
+                T=0
+            else:
+                time.sleep(wait*10) #wait 1e-05 before next commnad 
+                            
+
+                
 #Lazer off 0x06 is SPI byte following 0x03 to turn laser off.
 def LazOff(ser):
         print("Lazer Off")
         
-        ser.write(bytearray([0x61,0x03]))
-        nl = ser.read(2)
-        print(nl)
-        time.sleep(.1)
-        ser.write(bytearray([0x61,0x06])) #ON
-        nl = ser.read(2)
-        print(nl)
-        time.sleep(.1)
+        T=0 #Triese counter
+        while True:   
+            ser.write(bytearray([0x61,0x03]))
+            nl = ser.read(2)
+            print(nl)
+            T=T+1 
+            if nl== (b"\xff\xf3" or b"xf3\xff"):
+                time.sleep(wait)
+                #Lazer off
+                ser.write(bytearray([0x61,0x06]))
+                nl = ser.read(2)
+                print(nl)
+                time.sleep(wait)
+                Laz="Off"
+                print("Lazer Off")
+                return Laz
+            elif T > 20:
+                print("Reset SPI")
+                time.sleep(3) #time for spi buffer to reset
+                #reset SPI  conncetion 
+             #   initOPC(ser)
+                T=0
+            else:
+                time.sleep(wait*10) #wait 1e-05 before next commnad 
+                        
         
         
 def combine_bytes(LSB, MSB):
         return (MSB << 8) | LSB       
- 
+def Histdata(ans):
+#function for all the hist data, to break up the getHist
+     #time.sleep(wait)  
+   
+    data={}
+    data['Bin 0'] = combine_bytes(ans[0],ans[1])
+    data['Bin 1'] = combine_bytes(ans[2],ans[3])
+    data['Bin 2'] = combine_bytes(ans[4],ans[5])
+    data['Bin 3'] = combine_bytes(ans[6],ans[7])
+    data['Bin 4'] = combine_bytes(ans[8],ans[9])
+    data['Bin 5'] = combine_bytes(ans[10],ans[11])
+    data['Bin 6'] = combine_bytes(ans[12],ans[13])
+    data['Bin 7'] = combine_bytes(ans[14],ans[15])
+    data['Bin 8'] = combine_bytes(ans[16],ans[17])
+    data['Bin 9'] = combine_bytes(ans[18],ans[19])
+    data['Bin 10'] = combine_bytes(ans[20],ans[21])
+    data['Bin 11'] = combine_bytes(ans[22],ans[23])
+    data['Bin 12'] = combine_bytes(ans[24],ans[25])
+    data['Bin 13'] = combine_bytes(ans[26],ans[27])
+    data['Bin 14'] = combine_bytes(ans[28],ans[29])
+    data['Bin 15'] = combine_bytes(ans[30],ans[31])
+    data['Bin 16'] = combine_bytes(ans[32],ans[33])
+    data['Bin 17'] = combine_bytes(ans[34],ans[35])
+    data['Bin 18'] = combine_bytes(ans[36],ans[37])
+    data['Bin 19'] = combine_bytes(ans[38],ans[39])
+    data['Bin 20'] = combine_bytes(ans[40],ans[41])
+    data['Bin 21'] = combine_bytes(ans[42],ans[43])
+    data['Bin 22'] = combine_bytes(ans[44],ans[45])
+    data['Bin 23'] = combine_bytes(ans[46],ans[47])
+    data['Bin 24'] = combine_bytes(ans[48],ans[49])
+    data['period'] = combine_bytes(ans[52],ans[53])
+    data['FlowRate'] = combine_bytes(ans[54],ans[55])
+    data['Temp']=combine_bytes(ans[56],ans[57])
+    data['RH'] = combine_bytes(ans[52],ans[53])
+    data['pm1'] = struct.unpack('f',bytes(ans[60:64]))[0]
+    data['pm2.5'] = struct.unpack('f',bytes(ans[64:68]))[0]
+    data['pm10'] = struct.unpack('f',bytes(ans[68:72]))[0]
+    data['Check']= combine_bytes(ans[84],ans[85])
+      #  print(data)
+    return(data)
 
+def read_all(port, chunk_size=86):
+    """Read all characters on the serial port and return them."""
+    if not port.timeout:
+        raise TypeError('Port needs to have a timeout set!')
+
+    read_buffer = b''
+
+    while True:
+        # Read in chunks. Each chunk will wait as long as specified by
+        # timeout. Increase chunk_size to fail quicker
+        byte_chunk = port.read(size=chunk_size)
+        read_buffer += byte_chunk
+        if not len(byte_chunk) == chunk_size:
+            break
+
+    return read_buffer
+
+
+def getData(ser):
+        T=0
+        
+        br = bytearray([0x61])
+        for i in range(0,13):
+            br.append(0x32)
+        while True:
+            ser.write(bytearray([0x61,0x32]))
+            nl=ser.read(2)
+            
+            print(len(br)) 
+            if nl== (b"\xff\xf3" or b"xf3\xff"):
+                time.sleep(wait*10)
+                ser.write(br)
+                ans=bytearray(ser.read(13))
+                b1 = ans[0:4]
+                b2 = ans[4:8]
+                b3 = ans[8:12]
+                c1=struct.unpack('f',bytes(b1))[0]
+                c2=struct.unpack('f',bytes(b2))[0]
+                c3=struct.unpack('f',bytes(b3))[0]
+                return([c1,c2,c3])
+            elif T > 20:
+                print("Reset SPI")
+                time.sleep(3) #time for spi buffer to reset
+                #reset SPI  conncetion 
+                initOPC(ser)
+                T=0 
+                return
+            else:
+                time.sleep(wait*10) #wait 1e-05 before next commnad     
+#get hist data 
 def getHist(ser):
     
-        print("get hist")
-        ser.write(bytearray([0x61,0x30])) #reques the hist data set 
-        time.sleep(0.1)
-        nl=ser.read(2)
-        print("Does if have f3 ? ",nl)
-        time.sleep(.1) #delay
-        br = bytearray([0x61])
-        print("br",br)
-        for i in range(0,85):
+        #OPC N2 method 
+        T=0 #attemt varaible 
+        while True:   
+            print("get hist attempt ",T)
+            
+            #get the bytearray ready to send 
+            br = bytearray([0x61])
+            for i in range(0,85):
                 br.append(0x30)
-        print("br=",br,len(br))    
-        ser.write(br)
-        ans=bytearray(ser.read(1))
-        print("ans=",ans,"len",len(ans))
-        ans=bytearray(ser.read(86))
-        print("ans=",ans,"len",len(ans))
-        data={}
-        data['Bin 0'] = combine_bytes(ans[0],ans[1])
-        data['Bin 1'] = combine_bytes(ans[2],ans[3])
-        data['Bin 2'] = combine_bytes(ans[4],ans[5])
-        data['Bin 3'] = combine_bytes(ans[6],ans[7])
-        data['Bin 4'] = combine_bytes(ans[8],ans[9])
-        data['Bin 5'] = combine_bytes(ans[10],ans[11])
-        data['Bin 6'] = combine_bytes(ans[12],ans[13])
-        data['Bin 7'] = combine_bytes(ans[14],ans[15])
-        data['Bin 8'] = combine_bytes(ans[16],ans[17])
-        data['Bin 9'] = combine_bytes(ans[18],ans[19])
-        data['Bin 10'] = combine_bytes(ans[20],ans[21])
-        data['Bin 11'] = combine_bytes(ans[22],ans[23])
-        data['Bin 12'] = combine_bytes(ans[24],ans[25])
-        data['Bin 13'] = combine_bytes(ans[26],ans[27])
-        data['Bin 14'] = combine_bytes(ans[28],ans[29])
-        data['Bin 15'] = combine_bytes(ans[30],ans[31])
-        data['Bin 16'] = combine_bytes(ans[32],ans[33])
-        data['Bin 17'] = combine_bytes(ans[34],ans[35])
-        data['Bin 18'] = combine_bytes(ans[36],ans[37])
-        data['Bin 19'] = combine_bytes(ans[38],ans[39])
-        data['Bin 20'] = combine_bytes(ans[40],ans[41])
-        data['Bin 21'] = combine_bytes(ans[42],ans[43])
-        data['Bin 22'] = combine_bytes(ans[44],ans[45])
-        data['Bin 23'] = combine_bytes(ans[46],ans[47])
-        data['Bin 24'] = combine_bytes(ans[48],ans[49])
-        data['period'] = combine_bytes(ans[52],ans[53])
-        data['FlowRate'] = combine_bytes(ans[54],ans[55])
-        data['Temp']=combine_bytes(ans[56],ans[57])
-        data['RH'] = combine_bytes(ans[52],ans[53])
-        data['pm1'] = struct.unpack('f',bytes(ans[60:64]))[0]
-        data['pm2.5'] = struct.unpack('f',bytes(ans[64:68]))[0]
-        data['pm10'] = struct.unpack('f',bytes(ans[68:72]))[0]
-        data['Check']= combine_bytes(ans[84],ans[85])
-      #  print(data)
-        return(data)
-	
-# Retrieve data
-def getData(ser):
-        ser.write(bytearray([0x61,0x32]))
-        nl=ser.read(2)
-        time.sleep(.1)
-        ser.write(bytearray([0x61,0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32]))
-        ans=bytearray(ser.read(14))
-        b1 = ans[0:4]
-        b2 = ans[4:8]
-        b3 = ans[8:12]
-        print(b1,b2,b3)
-        #get vales in micro grams per cubic meter 
-        c1=struct.unpack('f',bytes(b1))[0]
-        c2=struct.unpack('f',bytes(b2))[0]
-        c3=struct.unpack('f',bytes(b3))[0]
-        return([c1,c2,c3])
-        
-def initFile(date):
-        ofile= FOLDER + LOCATION + '_' + OPCNAME + '_' + str(date).replace('-','') + ".csv"
-        print("Opening Output File:")
-        if(not os.path.isfile(ofile)):
-                f=open(ofile,'w+')
-                print("time,b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,period,pm1,pm2,pm10",file=f)
-        else:
-                f=open(ofile,'a')
-        return f
-
-
-
+            print("br",br,len(br))
+            
+            
+            #reques the hist data set 
+            ser.write(bytearray([0x61,0x30]))
+           # time.sleep(wait*10)
+            nl = ser.read(2)
+            time.sleep(.1) #delay
+            #time.sleep(wait*10)
+            
+             
+            
+            
+            print(nl)
+            T=T+1 
+            if nl== (b"\xff\xf3" or b"xf3\xff"):
+                print("Reading Hist data")
+                time.sleep(wait) #delay
+                test=ser.write(br) 
+                print("ser.write= ",test)
+                time.sleep(1) #delay
+             
+                ans=bytearray(ser.read(1))
+                print("ans=",ans,"len",len(ans))
+                time.sleep(wait) #delay
+                test=ser.readall()
+                ans=bytearray(test)
+                print("ans=",ans,"len",len(ans))
+                print("test=",test,'len',len(test))
+                data=Histdata(ans)
+                
+                return(data ,ans, br)
+            elif T > 20:
+                print("Reset SPI")
+                time.sleep(3) #time for spi buffer to reset
+                #reset SPI  conncetion 
+                initOPC(ser)
+                T=0
+             
+                return
+            else:
+                time.sleep(wait*10) #wait 1e-05 before next commnad 
+                        
+                        # br.append(0x30) 
+                        
+                #print("br",br,len(br))
+                
+                #older  vertion
+               # br = bytearray([0x61])
+              #  time.sleep(wait)
+               # for i in range(0,85):
+                #    br.append(0x30)   
+                #print(i,len(br),br)
+                
+                
+               
+    
+      
 
 
        
 if __name__ == "__main__":
     
-     
         serial_opts = {
         # built-in serial port is "COM1"
         # USB serial port is "COM4"
@@ -208,7 +345,10 @@ if __name__ == "__main__":
         "bytesize": serial.EIGHTBITS,
         "stopbits": serial.STOPBITS_ONE,
          "xonxoff": False,
-        "timeout": 1
+        "timeout":  1,
+        # "inter_byte_timeout":wait,# Alternative
+        # "write_timeout":wait,
+         
         }
 	
 	# wait for opc to boot
@@ -227,28 +367,30 @@ if __name__ == "__main__":
         initOPC(ser)
         time.sleep(1)
         print("Fan & Lazer Off:")
-        fanOff(ser)
-        LazOff(ser)
+        fan=fanOff(ser)
+        time.sleep(.1)
+        laz=LazOff(ser)
         time.sleep(5)
         print("Fan & Lazer on:")
-        fanOn(ser)
+        fan=fanOn(ser)
         time.sleep(5)
-       # LazOn(ser)
+        LazOn(ser)
        # time.sleep(5)
        # PM data#
-        for x in range(0,5):
+        for x in range(0,2):
         #   print(getData(ser))
-            #get Hist
-            LazOn(ser)
-            time.sleep(.1)
-            print(getHist(ser))
-            #ser.close()
-            LazOff(ser)
+            
+            print(getData(ser))
+          
             time.sleep(integration)  
         print("Turning off")   
-        time.sleep(1)                
-        LazOff(ser)  
-        fanOff(ser)  
+        time.sleep(2)       
+        ser.close()
+        time.sleep(wait)
+        ser.open()
+        LazOff(ser)
+        fanOff(ser)
+    
+      
         time.sleep(5)        
         ser.close()
-    
